c5adc31bc12dcf5df70ad2dc4b4175a10551ea56
gregor taube <gregjotau@gmail.com>
2019-04-18 20:27:34 +0200

    clean up and add headers

--- convert.py
+++ convert.py
@@ -1,17 +1,18 @@
 import csv
 
-row = ['2', ' Marie', ' California']
-row1 = ['2', ' molo', ' mololand']
-
 with open('transferwise.csv', 'r') as readFile:
     reader = csv.reader(readFile)
     lines = list(reader)
-    lines[2] = row
-    lines[1] = row1
+
+main_header = ['Konto', 'Kontonavn']
+saldo_header = ['Inngående saldo', 'Utgående saldo', 'Sum inn på konto', 'Sum ut av konto']
+data_header = ['Konto', 'Kontonavn']
+
+dnblines = [main_header]
 
 with open('dnb.csv', 'w') as writeFile:
     writer = csv.writer(writeFile)
-    writer.writerows(lines)
+    writer.writerows(dnblines)
 
 readFile.close()
 writeFile.close()
