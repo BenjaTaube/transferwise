18a14cf6a4a5f7d9b20adb0db680efd83f51542f
gregor taube <gregjotau@gmail.com>
2019-05-01 13:42:40 +0200

    fix script. formatting of dates, quotation and number formatting

--- convert.py
+++ convert.py
@@ -1,6 +1,6 @@
 import csv
 
-with open('transferwise.csv', 'r') as readFile:
+with open('transferwise-april.csv', 'r') as readFile:
     reader = csv.reader(readFile)
     transferwise_lines = list(reader)
 
@@ -8,16 +8,19 @@ main_header = ['Konto', 'Kontonavn']
 main_header_data = ['transferwise USD balance', 'transferwise USD balance']
 
 # In and outgoing balance calculation
-running_balance_after_first_payment = float(transferwise_lines[-1][6])
-first_payment_amount = float(transferwise_lines[-1][2])
+running_balance_after_first_payment = round(float(transferwise_lines[-1][6]), 2)
+first_payment_amount = round(float(transferwise_lines[-1][2]), 2)
 running_balance_before_first_payment = running_balance_after_first_payment - first_payment_amount
 
 running_balance_after_last_payment = transferwise_lines[1][6]
 
 saldo_header = ['Inngående saldo', 'Utgående saldo']
-saldo_header_data = [running_balance_before_first_payment, running_balance_after_last_payment]
+saldo_header_data = [
+    str(running_balance_before_first_payment).replace(".", ","),
+    str(running_balance_after_last_payment).replace(".", ",")
+]
 
-data_header = ['Bokført dato', 'Forklarende tekst', 'Inn', 'Ut']
+data_header = ['Bokført dato', 'Forklarende tekst', 'Ut', 'Inn']
 data_header_data = []
 transferwise_lines_data = transferwise_lines[1:]
 
@@ -28,22 +31,30 @@ dnblines = [
     saldo_header_data,
     data_header,
 ]
+
+transferwise_lines_data.reverse()
 for transaction_line in transferwise_lines_data:
-    date = transaction_line[1]
+    date = transaction_line[1].replace("-", ".")
     description = transaction_line[4]
     amount = transaction_line[2]
     if float(amount) > 0:
-        in_amount = amount
+        in_amount = amount.replace(".", ",")
         out_amount = None
+        dnblines.append([date, description, in_amount, out_amount])
     else:
         in_amount = None
-        out_amount = amount
-
-    dnblines.append([date, description, in_amount, out_amount])
-
+        out_amount = amount.replace(".", ",")
+        dnblines.append(
+            [
+                date,
+                description,
+                in_amount,
+                out_amount
+            ]
+        )
 
 with open('dnb.csv', 'w', newline='') as writeFile:
-    writer = csv.writer(writeFile,)
+    writer = csv.writer(writeFile, delimiter=';', quoting=csv.QUOTE_ALL)
     writer.writerows(dnblines)
 
 readFile.close()
